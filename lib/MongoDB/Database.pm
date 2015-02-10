db926d918d6bad71767a7bf049b09e61b0d3f83e
Marcel Timmerman <mt1957@gmail.com>
2015-02-10 20:33:12 +0100

    nop

--- lib/MongoDB/Database.pm
+++ lib/MongoDB/Database.pm
@@ -3,79 +3,88 @@ use MongoDB::Collection;
 
 class MongoDB::Database {
 
-    has $.connection is rw;
-    has Str $.name is rw;
+  has $.connection is rw;
+  has Str $.name is rw;
 
-    submethod BUILD ( :$connection, Str :$name ) {
+  #-----------------------------------------------------------------------------
+  #
+  submethod BUILD ( :$connection, Str :$name ) {
 
-        $!connection = $connection;
+      $!connection = $connection;
 
-        # TODO validate name
-        $!name = $name;
-    }
+      # TODO validate name
+      $!name = $name;
+  }
 
-    method collection ( Str $name --> MongoDB::Collection ) {
+  #-----------------------------------------------------------------------------
+  #
+  method collection ( Str $name --> MongoDB::Collection ) {
 
-        return MongoDB::Collection.new(
-            database    => self,
-            name        => $name,
-        );
-    }
+      return MongoDB::Collection.new(
+          database    => self,
+          name        => $name,
+      );
+  }
 
-    # Run command should ony be working on the admin database using the virtual
-    # $cmd collection. Method is placed here because it works on a database be
-    # it a special one.
-    #
-    # Possible returns are:
-    # %("ok" => 0e0, "errmsg" => <Some error string>)
-    # %("ok" => 1e0, ...);
-    #
-    method run_command ( %command --> Hash ) {
+  #-----------------------------------------------------------------------------
+  # Run command should ony be working on the admin database using the virtual
+  # $cmd collection. Method is placed here because it works on a database be
+  # it a special one.
+  #
+  # Possible returns are:
+  # %("ok" => 0e0, "errmsg" => <Some error string>)
+  # %("ok" => 1e0, ...);
+  #
+  method run_command ( %command --> Hash ) {
 
-        my MongoDB::Collection $c .= new(
-            database    => self,
-            name        => '$cmd',
-        );
+      my MongoDB::Collection $c .= new(
+          database    => self,
+          name        => '$cmd',
+      );
 
-        return $c.find_one(%command);
-    }
+      return $c.find_one(%command);
+  }
 
-    # Drop the database
-    #
-    method drop ( --> Hash ) {
+  #-----------------------------------------------------------------------------
+  # Drop the database
+  #
+  method drop ( --> Hash ) {
 
-        return self.run_command(%(dropDatabase => 1));
-    }
+      return self.run_command(%(dropDatabase => 1));
+  }
 
-    # Get the last error. Returns one or more of the following keys: ok, err,
-    # code, connectionId, lastOp, n, shards, singleShard, updatedExisting,
-    # upserted, wnote, wtimeout, waited, wtime,
-    #
-    method get_last_error ( Bool :$j = True, Int :$w = 0, Int :$wtimeout = 1000,
-                            Bool :$fsync = False
-                            --> Hash
-                          ) {
+  #-----------------------------------------------------------------------------
+  # Get the last error. Returns one or more of the following keys: ok, err,
+  # code, connectionId, lastOp, n, shards, singleShard, updatedExisting,
+  # upserted, wnote, wtimeout, waited, wtime,
+  #
+  method get_last_error ( Bool :$j = True, Int :$w = 0, Int :$wtimeout = 1000,
+                          Bool :$fsync = False
+                          --> Hash
+                        ) {
 
-        my %options = :$j, :$fsync;
-        if $w and $wtimeout {
-            %options<w> = $w;
-            %options<wtimeout> = $wtimeout;
-        }
+      my %options = :$j, :$fsync;
+      if $w and $wtimeout {
+          %options<w> = $w;
+          %options<wtimeout> = $wtimeout;
+      }
 
-        return self.run_command(%( getLastError => 1, %options));
-    }
+      return self.run_command(%( getLastError => 1, %options));
+  }
 
-    # Get errors since last reset error command
-    #
-    method get_prev_error ( --> Hash ) {
+  #-----------------------------------------------------------------------------
+  # Get errors since last reset error command
+  #
+  method get_prev_error ( --> Hash ) {
 
-        return self.run_command(%( getPrevError => 1));
-    }
+      return self.run_command(%( getPrevError => 1));
+  }
 
-    # Reset error command
-    #
-    method reset_error ( --> Hash ) {
+  #-----------------------------------------------------------------------------
+  # Reset error command
+  #
+  method reset_error ( --> Hash ) {
 
-        return self.run_command(%( resetError => 1));
-    }
+      return self.run_command(%( resetError => 1));
+  }
 }
