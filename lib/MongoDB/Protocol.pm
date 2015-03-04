15d87c41a50d7f693c4b3b4f82c2f2f783d9eedb
Marcel Timmerman <mt1957@gmail.com>
2015-03-04 11:59:59 +0100

    Bugfix in Protocol.pm

--- lib/MongoDB/Protocol.pm
+++ lib/MongoDB/Protocol.pm
@@ -5,5 +5,5 @@ role MongoDB::Protocol {
 
     state MongoDB::Wire $wp = MongoDB::Wire.new;
 
-    method ^wire ( --> MongoDB::Wire ) { return $wp };
+    method wire ( --> MongoDB::Wire ) { return $wp };
 };
