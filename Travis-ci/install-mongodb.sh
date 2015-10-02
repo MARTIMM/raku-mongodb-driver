b0563017c4c310877cc459df701b4d0f7f885a6b
Marcel Timmerman <mt1957@gmail.com>
2015-10-02 11:07:04 +0200

    improve travis shell script

--- Travis-ci/install-mongodb.sh
+++ Travis-ci/install-mongodb.sh
@@ -3,11 +3,29 @@
 # Install mongod of specified version, unpack and create a link to the bin
 # directory: ${TRAVIS_BUILD_DIR}/Travis-ci/MongoDB
 #
-echo Installing MongoDB version $1
 version=$1
 
+if [ ! $version ]
+then
+  echo "No version given to go for"
+  exit 1
+fi
+
+echo Installing MongoDB version $1
+
 cd ${TRAVIS_BUILD_DIR}/Travis-ci
-wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${version}.tgz
-tar xvfz mongodb-linux-x86_64-${version}.tgz
+if [ ! -e mongodb-linux-x86_64-${version}.tgz ]
+then
+  curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-${version}.tgz
+fi
+
+# Only get mongod server program
+#
+tar xvfz mongodb-linux-x86_64-${version}.tgz mongodb-linux-x86_64-${version}/bin/mongod
+
+if [ -e MongoDB ]
+then
+  rm MongoDB
+fi
 
 ln -s mongodb-linux-x86_64-${version}/bin MongoDB
