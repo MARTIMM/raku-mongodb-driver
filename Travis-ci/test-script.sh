2c1614e16d2f6f1f68b4ed4608e3163d58ac4193
Marcel Timmerman <mt1957@gmail.com>
2016-05-21 19:10:04 +0200

    next, sigh

--- Travis-ci/test-script.sh
+++ Travis-ci/test-script.sh
@@ -4,12 +4,12 @@ set -ev
 
 P6="${TRAVIS_BUILD_DIR}/Travis-ci/P6Software/rakudo/install"
 PATH="${P6}/bin:${P6}/share/perl6/site/bin:/usr/bin:/bin"
-#PERL6LIB="lib"
+PERL6LIB="lib"
 
 export PATH
-#export PERL6LIB
+export PERL6LIB
 
 pwd
-ls -l
+ls -l lib
 
 prove --verbose --recurse -Ilib --exec=perl6 t
