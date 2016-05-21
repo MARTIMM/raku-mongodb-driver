fdad81d0236dd318c93257c3202f64ed0bb59fea
Marcel Timmerman <mt1957@gmail.com>
2016-05-21 11:08:34 +0200

    test went wrong on finding modules

--- Travis-ci/test-script.sh
+++ Travis-ci/test-script.sh
@@ -4,11 +4,12 @@ set -ev
 
 P6="${TRAVIS_BUILD_DIR}/Travis-ci/P6Software/rakudo/install"
 PATH="${P6}/bin:${P6}/share/perl6/site/bin:/usr/bin"
-PERL6LIB="lib"
+#PERL6LIB="lib"
 
-echo
+export PATH
+#export PERL6LIB
+
 pwd
-echo
 
-prove -v -e perl6 t
+prove --verbose --recurse --lib --exec=perl6 t
 
